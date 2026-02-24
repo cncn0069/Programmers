021a4e990b255e4ac64a4f1640a14b58506d3364
cncn0069 <54030154+cncn0069@users.noreply.github.com>
2026-02-24 20:47:56 +0900

    [level 1] Title: 짝수와 홀수, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12937. 짝수와 홀수/짝수와 홀수.js
@@ -0,0 +1,11 @@
+function solution(num) {
+    var answer = '';
+    
+    if(num == 0){
+        return "Even"
+    }else if(num % 2 == 0){
+        return "Even"
+    }
+    
+    return "Odd";
+}
