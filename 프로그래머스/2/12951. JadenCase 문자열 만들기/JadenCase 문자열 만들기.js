8d8ed45281d6a42eb2cf97b8c6136eeb2bf80e8b
cncn0069 <54030154+cncn0069@users.noreply.github.com>
2026-03-28 10:16:21 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.09 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.js
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.js
@@ -1,16 +1,12 @@
 function solution(s) {
     var answer = '';
     
-    const str = s.split(' ')
+    //s 문자열을 받아 옵니다.
+    //"3people unFollowed me"
+    // [3people, unFollowed
+    //' '
+    answer = s.split(' ').map((i)=>
+                            i ? i[0].toUpperCase() + i.slice(1).toLowerCase()  : '' ).join(' ')
     
-    for(st of str){
-        let temp = st;
-        
-        if(temp.length > 0){
-            temp = temp[0].toUpperCase() + temp.slice(1).toLowerCase()
-        }
-        answer = answer.concat(" ", temp);
-    }
-    
-    return answer.trimStart();
+    return answer;
 }
--- 프로그래머스/2/12951. JadenCase 문자열 만들기/README.md
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/README.md
@@ -1,10 +1,10 @@
 # [level 2] JadenCase 문자열 만들기 - 12951 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12951?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12951) 
 
 ### 성능 요약
 
-메모리: 33.5 MB, 시간: 0.10 ms
+메모리: 33.4 MB, 시간: 0.09 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 03월 28일 06:52:22
+2026년 03월 28일 10:16:22
 
 ### 문제 설명
 
